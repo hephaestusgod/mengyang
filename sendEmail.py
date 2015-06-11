3790bc358f603a817240412cfe12b94db40bc4d6
hulatang <chenbinarctic@163.com>
2015-06-11 21:40:32 +0800

    read json file added

--- sendEmail.py
+++ sendEmail.py
@@ -5,21 +5,32 @@ from email.header import Header
 from email.utils import parseaddr, formataddr
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
+import json
 
 def _format_addr(s):
     name, addr = parseaddr(s)
     return formataddr((Header(name, 'utf-8').encode(), addr))
 
-from_addr = ''
-pw = ''
+#account_info = json.loads(open('data.text','rb').read())
 
-to_addr1 = ''
-to_addr2 = ''
+with open('data.text','rb') as f:
+    account_info = json.loads(f.read())
 
+
+
+
+from_addr = account_info['from_addr']['username'] 
+pw = account_info['from_addr']['password']
+
+
+to_addr1 = account_info['to_addr']['to_addr1']['email'] 
+to_addr2 = account_info['to_addr']['to_addr2']['email']
+
+from_name = account_info['from_addr']['name']
 
 #msg = MIMEText('hello, Meng Yang', 'plain', 'utf-8')
 msg = MIMEMultipart()
-msg['From'] = _format_addr('bin chen <%s>' % from_addr)
+msg['From'] = _format_addr('%s <%s>'  %(from_name,from_addr))
 msg['To'] = _format_addr('admin <%s>' % to_addr2)
 msg['Subject'] = Header('Data From Ju Mei', 'utf-8').encode()
 
@@ -27,7 +38,7 @@ msg.attach(MIMEText('send with file', 'plain', 'utf-8'))
 
 #with open('/Users/binchen/dev/jumei/data/JuMei.xls', 'rb') as f:
 
-with open('/home/eric/dev/jumei/JuMei.xls', 'rb') as f:
+with open('JuMei.xls', 'rb') as f:
     mime = MIMEBase('xls', 'xls', filename='JuMei.xls')
 
     mime.add_header('Content-Disposition', 'attachment', filename='JuMeil.xls')
